358ca7fb2ee0cbb19884a01833be1e9c6ff2844c
maciejors <maciej.orslowski@gmail.com>
2023-04-25 18:23:01 +0200

    implement Pipeline class

--- src/modules/pipeline.py
+++ src/modules/pipeline.py
@@ -11,112 +11,113 @@ import src.modules.learner_module as lm
 import src.modules.evaluation_module as em
 
 
-# class Pipeline():
-    
-#     __slots__ = ['data_set', 'learner', 'optimizer', 
-#                  'loss_function', 'n_queries', 'init_epochs', 
-#                  'epochs_per_query', 'query_batch_size', 'test_loader', 'train_loader']
-
-#     def __init__(self):
-#         self.data_set = None
-#         self.learner = None
-#         self.optimizer = None
-#         self.loss_function = None
-#         self.n_queries = None
-#         self.init_epochs = None
-#         self.epochs_per_query = None
-#         self.query_batch_size = None
-
-#     def fit(self, data_set:dm.ActiveDataset, learner:lm.ActiveLearner,
-#             optimizer, loss_function, n_queries, init_epochs, epochs_per_query,
-#             query_batch_size = 10):
-#         self.test_loader = DataLoader(data_set.test_set, batch_size=128)
-#         self.train_loader = DataLoader(data_set.labeled_set, batch_size=32, shuffle=True)
-#         self.data_set = data_set
-#         self.learner = learner
-#         self.optimizer = optimizer
-#         self.loss_function = loss_function
-#         self.n_queries = n_queries
-#         self.init_epochs = init_epochs
-#         self.epochs_per_query = epochs_per_query
-#         self.query_batch_size = query_batch_size
-    
-#     def transform(self):
-#         t_hist_all, v_hist_all = self.learner.fit(self.train_loader, self.test_loader,
-#                                                     self.optimizer, self.loss_function, 
-#                                                     early_stopping=True, epochs=self.init_epochs)
-#         a_hist_all = []
-#         a_hist_all.append(self._calculate_accuracy(self.test_loader).item())
-
-#         for i in range(self.n_queries):
-#             print(f"Iteration: {i+1}")
-            
-#             # generate queries
-#             unlabeled_loader = DataLoader(self.data_set.unlabeled_set, batch_size=128, shuffle=False)
-#             queries, uncertainty = self.learner.generate_query(unlabeled_loader, criterion='entropy', batch_size=self.query_batch_size)
-#             self.data_set.get_label_by_idx(queries, move_sample=True)
-#             print(f"Max uncertainty: {uncertainty}")
-            
-#             self.train_loader = DataLoader(self.data_set.labeled_set, batch_size=32, shuffle=True)
-
-#             t_hist, v_hist = self.learner.fit(self.train_loader, self.test_loader, self.optimizer,
-#                                                self.loss_function, epochs=self.epochs_per_query, 
-#                                                early_stopping=True, sample_weights=None)
-                                        
-#             # save history
-#             t_hist_all.extend(t_hist)
-#             v_hist_all.extend(v_hist)
-#             a_hist_all.append(self._calculate_accuracy(self.learner, self.test_loader).item())
-#         #maybe better to save this arrays to .txt flat file cuz in evaluating pipeline in the loop will lost last arrays
-#         return t_hist_all, v_hist_all, a_hist_all
-
-#     def fit_transform(self, data_set:dm.ActiveDataset, learner:lm.ActiveLearner,
-#             optimizer, loss_function, n_queries, init_epochs, epochs_per_query,
-#             query_batch_size = 10):
-#             self.fit(data_set, learner, optimizer, loss_function, n_queries, init_epochs, 
-#                      epochs_per_query, query_batch_size)
-#             return self.transform()
-            
-#     def _calculate_f1_score(self, test_loader):
-#         outputs = self.learner.predict(test_loader)
-#         return multiclass_f1_score(outputs, test_loader.dataset.dataset.targets[test_loader.dataset.indices])
-
-#     def _calculate_accuracy(self, test_loader):
-#         outputs = self.learner.predict(test_loader)
-#         return multiclass_accuracy(outputs, test_loader.dataset.dataset.targets[test_loader.dataset.indices])
-    
-
-class Pipeline():
+class Pipeline:
      
-    __slots__ = ['ds', 'learner', 'settings', 'optimiser', 'loss_function']
+    __slots__ = ['dataset', 'learner', 'settings', 'optimiser', 'loss_function',
+                 'train_loader', 'test_loader']
     
-    def __init__(self, ds, learner, optimiser, loss_function,
-                 settings: Union[dict, PipelineSettings]) -> None:
-        self.ds = ds
+    def __init__(self, dataset: dm.ActiveDataset, learner: lm.ActiveLearner,
+                 optimiser, loss_function, settings: Union[dict, PipelineSettings]):
+        self.dataset = dataset
         self.learner = learner
         self.optimiser = optimiser
         self.loss_function = loss_function
+
+        self.test_loader = DataLoader(dataset.test_set, batch_size=128)
+        self.train_loader = DataLoader(dataset.labeled_set, batch_size=32, shuffle=True)
 
         if type(settings) == dict:
             settings = PipelineSettings.from_dict(settings)
         self.settings: PipelineSettings = settings
 
-    def run(self) -> em.Session:
-        # perform initial learner fit
-        # loop for n_queries
-        #       get_queries
-        #       label_queries
-        #       fit learner on updated train set
-        #       update session
-        # return session
-        pass
+    def run(self) -> tuple[em.Session, dict[str, list[float]]]:
+        """
+        Runs the pipeline. Return a Session object as well as a dict of stats, which contains
+        loss history on train and test and accuracy history
+
+        Pseudocode:
+
+        perform initial learner fit
+        loop for n_queries
+              get_queries
+              label_queries
+              fit learner on updated train set
+              update session
+        return session
+        """
+        # initialise loss/accuracy history arrays
+        # initial learner fit
+        t_loss_history, v_loss_history = self.learner.fit(
+            self.train_loader, self.test_loader,
+            self.optimiser, self.loss_function,
+            epochs=self.settings.init_epochs,
+            early_stopping=True,
+        )
+        accuracy_history = [self._calculate_accuracy(self.test_loader).item()]
+
+        # initialise the session
+        session = em.Session(dataset=self.dataset, learner=self.learner)
+
+        for i in range(self.settings.n_queries):
+            print(f"Iteration: {i+1}")
+
+            # generate queries
+            unlabeled_loader = DataLoader(self.dataset.unlabeled_set, batch_size=128, shuffle=False)
+            queries, uncertainty = self.learner.generate_query(
+                unlabeled_loader,
+                criterion='entropy',
+                batch_size=self.settings.query_batch_size,
+            )
+            # label queries
+            self.dataset.get_label_by_idx(queries, move_sample=True)
+            print(f"Max uncertainty: {uncertainty}")
+            self.train_loader = DataLoader(self.dataset.labeled_set, batch_size=32, shuffle=True)
+
+            # fit learner on updated train set
+            t_hist, v_hist = self.learner.fit(
+                self.train_loader, self.test_loader,
+                self.optimiser, self.loss_function,
+                epochs=self.settings.epochs_per_query,
+                early_stopping=True,
+                sample_weights=None,
+            )
+            # update session
+            session.update()
+
+            # update loss/accuracy history
+            t_loss_history.extend(t_hist)
+            v_loss_history.extend(v_hist)
+            accuracy_history.append(self._calculate_accuracy(self.test_loader).item())
+
+        # pack loss/accuracy history arrays into one dict
+        stats_to_return = {
+            't_loss_history': t_loss_history,
+            'v_loss_history': v_loss_history,
+            'accuracy_history': accuracy_history,
+        }
+        return session, stats_to_return
+
+    def _calculate_f1_score(self, test_loader):
+        outputs = self.learner.predict(test_loader)
+        return multiclass_f1_score(
+            outputs,
+            test_loader.dataset.dataset.targets[test_loader.dataset.indices]
+        )
+
+    def _calculate_accuracy(self, test_loader):
+        outputs = self.learner.predict(test_loader)
+        return multiclass_accuracy(
+            outputs,
+            test_loader.dataset.dataset.targets[test_loader.dataset.indices]
+        )
 
 
 class PipelineSettings:
 
     __slots__ = ['n_queries', 'init_epochs', 'epochs_per_query', 'query_batch_size']
 
-    def __init__(self, n_queries, init_epochs, epochs_per_query, query_batch_size):
+    def __init__(self, n_queries: int, init_epochs: int,
+                 epochs_per_query: int, query_batch_size: int = 100):
         self.n_queries = n_queries
         self.init_epochs = init_epochs
         self.epochs_per_query = epochs_per_query
